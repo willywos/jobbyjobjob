8a2747a4ac4b597718f5a9eb8317934190229405
willywos <wwilimek@prositions.com>
2018-09-08 01:43:29 -0500

    Tweaking search results so its a little more accurate and sorts by publish date with most recent first

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -9,7 +9,7 @@ class RootController < ApplicationController
   end
 
   def search
-    @job_postings = params[:query].blank? ? JobPosting.all.order("publish_date desc") : JobPosting.search_by_title(params[:query])
+    @job_postings = params[:query].blank? ? JobPosting.all.order("publish_date desc") : JobPosting.search_sort_by_pub_date(params[:query])
   end
 
   def view_post
--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -5,12 +5,19 @@ class JobPosting < ApplicationRecord
   pg_search_scope :search_by_title,
                   :against => {
                     :title => 'A',
+                    :company => 'B'
                   },
                   :using => {
-                    :tsearch => { dictionary: 'english', :any_word => true, :prefix => true}
+                    :tsearch => { dictionary: 'english', :any_word => true, :prefix => true, :normalization => 2}
                   },
                   :order_within_rank => "job_postings.publish_date DESC"
 
+
+  def self.search_sort_by_pub_date(searchTerm)
+    results = JobPosting.search_by_title(searchTerm)
+    results = results.sort { |x,y| y.publish_date <=> x.publish_date }
+    results
+  end
 
   def company_initials
     unless self.company.blank?
