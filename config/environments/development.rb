cc3da0dc95dbf90480330b9df6db933113ec4adb
Jake Sorce <jake.sorce@podium.com>
2018-10-02 09:00:47 -0600

    add ability to login and save jobs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  # Letter opener config
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
