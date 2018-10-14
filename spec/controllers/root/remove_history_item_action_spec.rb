f28724a50e4343d75820290cd090f613329d7a8f
Anderson Fernandes <fernandesanderson14@gmail.com>
2018-10-14 11:32:26 -0300

    feature/29 Add remove action to root_controller
    - add action to routes
    - add specs
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ spec/controllers/root/remove_history_item_action_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe RootController, '#remove_history_item', type: :controller do
+
+  let(:request_id) { '12' }
+  before do
+    session[:viewed_post_history] = [{'id' => 12, 'title' => 'Awesome Job'}]
+    delete 'remove_history_item', params: { id: request_id }, xhr: true
+  end
+
+  it 'response should be js' do
+    expect(response.content_type).to eq Mime[:js]
+  end
+
+  context 'with a valid request id' do
+    it 'session should be clean after the action' do
+      expect(session[:viewed_post_history]).to be_empty
+    end
+  end
+
+  context 'with an invalid request id' do
+    let(:request_id) { '25' }
+
+    it 'session should be clean after the action' do
+      expect(session[:viewed_post_history]).not_to be_empty
+    end
+  end
+end
