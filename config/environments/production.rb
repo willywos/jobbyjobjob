4673f291176707f8400f5048dcb8f529d620ab58
willywos <wwilimek@gmail.com>
2018-10-16 23:54:51 -0500

    Adding a default host for the mailer to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'jobbyjobjob.net' }
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
