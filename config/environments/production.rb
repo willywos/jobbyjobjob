55bf2def89e22e008a5e654974025a795380dea4
willywos <wwilimek@gmail.com>
2018-10-16 23:50:57 -0500

    Adding sengrid email settings for sending password reset email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'jobbyjobjob.net',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
