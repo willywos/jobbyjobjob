34569a800fdb1c0b8567e8ad93fdeb7c0af0e259
Ovidiu Alexandru Toma <inoxovi@gmail.com>
2018-10-29 12:25:08 +0200

    adds more tests for resume

--- spec/controllers/resumes_controller_spec.rb
+++ spec/controllers/resumes_controller_spec.rb
@@ -26,9 +26,33 @@ RSpec.describe ResumesController, type: :controller do
     expect(response.status).to eq(200)
   end
 
-  it "testes #edit access for @user2" do 
+  it "testes #edit access for @user2 - redirected" do 
     sign_in @user2
     get :edit, params: {id: @resume.id}
+    expect(response.status).to eq(302)
+  end
+
+  it "testes #update access for @user" do 
+    sign_in @user
+    patch :update, params: {id: @resume.id, resume: {name: "abc"}}
+    expect(response).to redirect_to(resumes_url) 
+  end
+
+  it "testes #update access for @user2 - redirected" do 
+    sign_in @user2
+    patch :update, params: {id: @resume.id, resume: {name: "abc"}}
+    expect(response).to redirect_to(root_url) 
+  end
+
+  it "testes #destroy access for @user" do 
+    sign_in @user
+    delete :destroy, params: {id: @resume.id, format: "script"}
+    expect(response.status).to eq(200)
+  end
+
+  it "testes #destroy access for @user2 - redirected" do 
+    sign_in @user2
+    delete :destroy, params: {id: @resume.id, format: "script"}
     expect(response.status).to eq(302)
   end
 
